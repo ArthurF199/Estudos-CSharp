08b9149dfa836031d31d049e2a49c90e90c76cc4
Arthur Fernandes <109669509+ArthurF199@users.noreply.github.com>
2026-03-17 20:54:22 -0300

    Exercício 3

---
+++ Exercícios Gerais/Exercícios/ex03.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercícios_Gerais.Exercícios
+{
+    internal class ex03
+    {
+        public static void Executar()
+        {   
+            
+        }   
+    }
+}
