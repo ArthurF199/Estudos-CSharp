c61f3fdc8cf978b2d82dff310d3fee1ab940c28e
ArthurF199 <109669509+ArthurF199@users.noreply.github.com>
2026-07-01 21:17:55 -0300

    Exercício do dia, apenas para relembrar conceitos básicos do C#

---
+++ Exercícios Gerais/Exercícios/ex12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercícios_Gerais.Exercícios
+{
+    internal class ex12
+    {
+        public static void Executar()
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            int idade = int.Parse(Console.ReadLine());
+            int altura = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+
+        }
+    }
+}
--- Exercícios Gerais/Program.cs
+++ Exercícios Gerais/Program.cs
@@ -1,3 +1,3 @@
 ﻿using Exercícios_Gerais.Exercícios;
 
-ex10.Executar();
+ex12.Executar();
